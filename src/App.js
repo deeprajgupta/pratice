bd634b37c5020c1c66e336d1fcca9fa15e7ffdee
deepraj <deeprajgupta143@gmail.com>
2021-12-03 12:25:15 +0530

    Revert "second commit"
    This reverts commit 02655e69a871710ea7ab6b88b8941ac27d65a6a7.
    
    testing just uncommiy process

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactsssssssssssss
+          Learn React
         </a>
       </header>
     </div>
